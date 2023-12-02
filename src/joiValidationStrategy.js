c23c7aec246f8cc734900d9a99645c706127670a
Kris Madison <jkmadison@me.com>
2023-12-02 09:00:05 -0500

    Changed joi validate call

--- src/joiValidationStrategy.js
+++ src/joiValidationStrategy.js
@@ -1,31 +1,35 @@
-import Joi from 'joi';
-import set from 'lodash.set';
-import get from 'lodash.get';
-import isEmpty from 'lodash.isempty';
-import compose from 'lodash.compose';
-import invariant from 'invariant';
-import { hydrate, decodeMessages, collectErrors, pickMessages } from './utils';
+import Joi from "joi";
+import set from "lodash.set";
+import get from "lodash.get";
+import isEmpty from "lodash.isempty";
+import compose from "lodash.compose";
+import invariant from "invariant";
+import { hydrate, decodeMessages, collectErrors, pickMessages } from "./utils";
 
-export default joiOptions => {
+export default (joiOptions) => {
   return {
     validate(data = {}, joiSchema = {}, options = {}, callback) {
-      invariant(typeof callback === 'function', 'joi-validation-strategy is asynchronous, a callback is expected: validate(data, schema, options, callback)');
-      const {
-        key,
-        prevErrors = {},
-      } = options;
+      invariant(
+        typeof callback === "function",
+        "joi-validation-strategy is asynchronous, a callback is expected: validate(data, schema, options, callback)"
+      );
+      const { key, prevErrors = {} } = options;
       const validationOptions = {
         abortEarly: false,
         allowUnknown: true,
         ...joiOptions,
       };
-      Joi.validate(data, joiSchema, validationOptions, error => {
-        const errors = compose(hydrate, pickMessages, collectErrors, decodeMessages)(error);
-        if (key === undefined || key === null || isEmpty(errors)) {
-          return callback(errors);
-        }
-        return callback(set(prevErrors, key, get(errors, key)));
-      });
+      const { error } = joiSchema.validate(data, validationOptions);
+      const errors = compose(
+        hydrate,
+        pickMessages,
+        collectErrors,
+        decodeMessages
+      )(error);
+      if (key === undefined || key === null || isEmpty(errors)) {
+        return callback(errors);
+      }
+      return callback(set(prevErrors, key, get(errors, key)));
     },
   };
 };
